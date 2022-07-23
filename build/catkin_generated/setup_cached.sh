e6a8917acf0d5a515c83531c45c5c7a08c920340
lws <937240243@qq.com>
2022-07-23 23:01:51 +0800

    remove builde

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/lws/catkin_ws/src/can_imu_rviz_plugin/build/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/lws/catkin_ws/src/can_imu_rviz_plugin/build/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/lws/catkin_ws/src/can_imu_rviz_plugin/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/lws/catkin_ws/src/can_imu_rviz_plugin/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/lws/catkin_ws/src/can_imu_rviz_plugin/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/lws/catkin_ws/src/can_imu_rviz_plugin/can_imu_rviz_plugin:$ROS_PACKAGE_PATH"
